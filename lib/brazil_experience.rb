22f36805c673360c48d511ff3f60db20eb1647b4
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-13 20:52:42 +0000

    added CLI file

--- lib/brazil_experience.rb
+++ lib/brazil_experience.rb
@@ -1,12 +1,17 @@
+#will act as environment
+
 require "open-uri"
 require "nokogiri"
+require "colorize"
 require "pry"
 
 require "./lib/brazil_experience/version"
 
 # require_relative "brazil_experience/version"
 
-module BrazilExperience
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module BrazilExperience
+#   class Error < StandardError; end
+# end
+
+require_relative "./brazil_experience/cli"
+
---
+++ lib/brazil_experience/cli.rb
@@ -0,0 +1,43 @@
+#our CLI controller, programming everything here
+
+class BrazilExperience::CLI 
+  
+  def begin_experience
+    puts "Welcome to the Brazil Experience!".yellow.bold
+      sleep 3
+    puts "What would you like to explore?".green.bold
+      sleep 2
+      main_menu
+    end
+      
+  def main_menu
+      puts "Please choose from: 
+        attractions or gastronomy".green.bold
+        
+        #   input = gets.strip
+        # if input == attractions
+        #   attractions
+        # elsif input == gastronomy
+        #   gastronomy
+        # else
+        #   puts  "I did not understand your choice. Please try again."
+          # main_menu
+        end
+  end
+  
+  # def attractions
+  #     puts "Which attraction would you like to learn more about?".green.bold
+  #       sleep 2 
+  #     puts "Please select a number:"
+  #       sleep 1
+  #   end
+  
+  # def gastronomy
+  #   puts "Which item would you like to learn more about?"
+  #     sleep 3
+  #   puts "Please choose by number:".green.bold
+  # end
+  
+  
+end
+
