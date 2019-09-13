6efee41fe12c737bc367131ee4c7a86ff39bfccb
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-13 01:11:03 +0000

    changes to scraper - defined content to save

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,13 +2,19 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-
-doc = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/places"))
-
-
-#location = doc.css(".content-card-text")[0].text.split("\n")[1]
-#attraction_name = doc.css(".content-card-text")[0].text.split("\n")[2]
-attraction_description = doc.css(".content-card-text")[0].text.split("\n")[3]
-
+    doc = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/places"))
+    
+    
+    
+    #to change attraction - only change first index - second index dicates location of information in content
+    location = doc.css(".content-card-text")[2].text.split("\n")[1]
+    #city of first attraction
+    
+    name = doc.css(".content-card-text")[2].text.split("\n")[2]
+   #name of first attraction
+  
+    description = doc.css(".content-card-text")[2].text.split("\n")[3]
+     #description of first attraction
+ 
 
 binding.pry
