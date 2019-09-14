2efdc3d83c0737441c69bd1b30b18a43011f95d8
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-14 23:41:06 +0000

    started scraper info for gastronomy

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,19 +2,26 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-    doc = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/places"))
+  #   doc_attraction = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/places"))
+
+  #   #to change attraction - only change first index - second index dicates location of information in content
+  #   location = doc.css(".content-card-text")[0].text.split("\n")[1]
+  #   #city of first attraction
     
-    
-    
-    #to change attraction - only change first index - second index dicates location of information in content
-    location = doc.css(".content-card-text")[0].text.split("\n")[1]
-    #city of first attraction
-    
-    name = doc.css(".content-card-text")[0].text.split("\n")[2]
-   #name of first attraction
+  #   name = doc.css(".content-card-text")[0].text.split("\n")[2]
+  # #name of first attraction
   
-    description = doc.css(".content-card-text")[0].text.split("\n")[3]
-     #description of first attraction
+  #   description = doc.css(".content-card-text")[0].text.split("\n")[3]
+  #   #description of first attraction
  
+ 
+ doc_gastronomy = Nokogiri::HTML(open("https://www.atlasobscura.com/unique-food-drink/brazil"))
+
+    
+    name = doc.css()
+    #name of food 
+    
+    description = doc.css()
+    #short description of food
 
 binding.pry
