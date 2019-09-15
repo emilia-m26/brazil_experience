38249798dd122f4dd8355e7c6492324fa0753885
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-15 19:44:25 +0000

    changes to classes, deleted info not needed

--- lib/brazil_experience/attractions.rb
+++ lib/brazil_experience/attractions.rb
@@ -21,13 +21,6 @@ class BrazilExperience::Attractions
     #should return instances of list of attractions to explore
     puts "These are your attraction choices."
     
-    attraction_1 = self.new 
-    attraction_1.name = "Largo do Boticario"
-    attraction_1.location = "Rio de Janeiro, Brazil"
-    attraction_1.description = "Short Description of Attraction"
-    
-    attraction_1.url = "https://www.atlasobscura.com/places/largo-do-boticario"
-    
   end
   
   
--- lib/brazil_experience/gastronomy.rb
+++ lib/brazil_experience/gastronomy.rb
@@ -6,14 +6,7 @@ class BrazilExperience::Gastronomy
     #should return instances of list of food items to explore
     puts "These are your food choices."
     
-    gastronomy_1 = self.new 
-    gastronomy_1.name = "Ice Cream Bean"
-    gastronomy_1.description = "Short Description of Food"
-    
-    gastronomy_1.url = "https://www.atlasobscura.com/foods/ice-cream-bean"
-    
-    [gastronomy_1...gastronomy_16]
-  end
+ 
   
   
 end
