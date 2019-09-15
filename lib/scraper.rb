6ada5a35189e9f30987db1eb8ca90f4ccbe8b800
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-15 00:38:38 +0000

    changes to scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-    doc_attraction = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/places"))
+    doc = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/places"))
 
     #to change attraction - only change first index - second index dicates location of information in content
     location = doc.css(".content-card-text")[0].text.split("\n")[1]
@@ -22,5 +22,8 @@ require "pry"
     
     description = doc_gastronomy.css(".content-card-text")[-1].text.split("\n")[6]
     #short description of food
+    
+   detail_food = doc_gastronomy.css(".content-card food-card.href=/foods/ice-cream-bean")
+  
   
 binding.pry
