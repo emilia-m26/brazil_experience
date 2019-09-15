bbb1ee111b4475e7ab4d4895037e856aca61915f
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-15 22:58:15 +0000

    defined iteration to list attraction by index+1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,27 +14,27 @@ def self.scrape_attractions
       location = attraction.css(".detail-sm.place-card-location").text
       description = attraction.css(".subtitle-sm").text
      
-      Attractions.new(name,location,description)
+      BrazilExperience::Attractions.new(name,location,description)
     
 #     BrazilExperience::Attractions.new(name, location, description)
     end
 
   end
   
-    def self.scrape_gastronomy
+  #   def self.scrape_gastronomy
       
-      gastronomy_page = "https://www.atlasobscura.com/unique-food-drink/brazil"
-      doc = Nokogiri::HTML(open(gastronomy_page))
+  #     gastronomy_page = "https://www.atlasobscura.com/unique-food-drink/brazil"
+  #     doc = Nokogiri::HTML(open(gastronomy_page))
 
-      doc.css(".content-card-text").each do |food|
+  #     doc.css(".content-card-text").each do |food|
         
-      name= food.css(".title-underline").text
-      description = food.css(".content-card-subtitle").text
-    #binding.pry 
+  #     name= food.css(".title-underline").text
+  #     description = food.css(".content-card-subtitle").text
+  #   #binding.pry 
      
-      Gastronomy.new(name, description)
-  end
+  #     Gastronomy.new(name, description)
+  # end
   
-  end
+  # end
  end
   
