b8e527e91a3e266645491f751a60437823f6b8c5
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-15 21:52:27 +0000

    making changes to gastronomy scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,33 +4,41 @@ require "pry"
 
 class Scraper
   
- def self.scrape_attractions
-    attractions_page = "https://www.atlasobscura.com/things-to-do/brazil/places"
-    doc = Nokogiri::HTML(open(attractions_page))
+# def self.scrape_attractions
+#     attractions_page = "https://www.atlasobscura.com/things-to-do/brazil/places"
+#     doc = Nokogiri::HTML(open(attractions_page))
     
     
-    doc.css(".content-card-text").each do |attraction|
-       name= attraction.css(".title-underline").text
-      location = attraction.css(".detail-sm.place-card-location").text
-      description = attraction.css(".subtitle-sm").text
+#     doc.css(".content-card-text").each do |attraction|
+#       name= attraction.css(".title-underline").text
+#       location = attraction.css(".detail-sm.place-card-location").text
+#       description = attraction.css(".subtitle-sm").text
      
-      Attractions.new(name,location,description)
+#       Attractions.new(name,location,description)
     
-#     BrazilExperience::Attractions.new(name, location, description)
-    end
+# #     BrazilExperience::Attractions.new(name, location, description)
+#     end
 
-  end
+#   end
   
-#   def self.scrape_gastronomy
-#     url = "https://www.atlasobscura.com/unique-food-drink/brazil"
-#     doc_gastronomy = Nokogiri::HTML(open(url))
-
+    def self.scrape_gastronomy
+      
+      gastronomy_page = "https://www.atlasobscura.com/unique-food-drink/brazil"
+      doc = Nokogiri::HTML(open(gastronomy_page))
+    
+    
+      doc.css(".content-card-text").each do |food|
+       name= food.css(".title-underline").text
+       description = food.css(".subtitle-sm").text
+     
+      Gastronomy.new(name,location,description)
 #     name = doc_gastronomy.css(".content-card-text")[0].text.split("\n")[4]
 #     #name of food 
     
 #     description = doc_gastronomy.css(".content-card-text")[0].text.split("\n")[6]
 #     #short description of food
 #   end
-
+  
+  end
  end
   
