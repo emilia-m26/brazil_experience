3c81c23b7cd6c4acf5920c11cfaa6f14d305875c
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-15 22:30:33 +0000

    changes to cli

--- lib/brazil_experience/gastronomy.rb
+++ lib/brazil_experience/gastronomy.rb
@@ -1,6 +1,21 @@
 class BrazilExperience::Gastronomy
   attr_accessor :name, :description
   
+  @@all=[]
+  
+  def initialize(name, description)
+    @name = name
+    @description = description
+    save  
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
   
   def self.list_gastronomy
     #should return instances of list of food items to explore
