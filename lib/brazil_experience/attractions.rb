57d194c0c0ee0d0b483c211022b2ba9851a2b1b2
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-15 02:08:47 +0000

    added Gastronomy and Attractions classes

---
+++ lib/brazil_experience/attractions.rb
@@ -0,0 +1,8 @@
+class BrazilExperience::Attractions
+  
+  def self.list_attractions
+    #should return list of attractions to explore
+  end
+  
+  
+end
---
+++ lib/brazil_experience/gastronomy.rb
@@ -0,0 +1,9 @@
+class BrazilExperience::Gastronomy
+  
+  
+  def self.list_gastronomy
+    #should return list of food items to explore
+  end
+  
+  
+end
