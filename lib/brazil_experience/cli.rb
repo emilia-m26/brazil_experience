f2466f9c4a300ebcc5d84198f1ab30361ce8cdda
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-16 21:49:56 +0000

    still testing loops, not fixed

--- lib/brazil_experience/cli.rb
+++ lib/brazil_experience/cli.rb
@@ -8,7 +8,6 @@ class BrazilExperience::CLI
     puts "Welcome to".green.bold + " the Brazil".blue.bold + " Experience!".yellow.bold
       sleep 3
     main_menu
-    #goodbye 
     end
       
   def main_menu
@@ -34,27 +33,28 @@ class BrazilExperience::CLI
   end
   
   def attractions
-    puts "These are the attractions available to explore. Which one would you like to learn more about?"
+    puts "These are the attractions available. Which one would you like to learn more about?"
       sleep 1
+      #lists attractions
       BrazilExperience::Attractions.all.each_with_index do |attraction,index|
         puts "#{index + 1}. #{attraction.name}"
       end
         sleep 1
         
-    puts "Please choose by number, main menu or exit:"
+    puts "Please choose a number, main menu or exit:"
         answer = gets.strip.downcase
         attraction_number = BrazilExperience::Attractions.all[answer.to_i-1]
     
-        case answer
-          when "attraction_number"
-              sleep 2
+        #case answer
+          if answer == attraction_number
             puts "*~* #{attraction_number.name} is located in #{attraction_number.location} *~*"
             puts  attraction_number.description
+            binding.pry
               sleep 4
             attractions
-          when "main menu"
+          elsif answer == "main menu"
             main_menu
-          when "exit"
+          elseif answer == "exit"
             sleep 1
             goodbye
           else 
@@ -62,6 +62,7 @@ class BrazilExperience::CLI
             sleep 2
           attractions
         end
+        
         
     #     #list of attractions to choose from by number input
     #     @list_attractions = BrazilExperience::Attractions.list_attractions
@@ -77,16 +78,29 @@ class BrazilExperience::CLI
      sleep 1
     puts "Please choose by number:".yellow.bold
         answer = gets.strip.downcase
-        if answer.to_i > BrazilExperience::Gastronomy.all.size || answer.to_i <= 0
-          puts "I did not understand your selection.  Please try again."
-          gastronomy
-        end
-  
         food_item = BrazilExperience::Gastronomy.all[answer.to_i-1]
+        
         puts "What is a(n) #{food_item.name}?"
-        puts food_item.description
-          sleep 3 
-          gastronomy
+            puts food_item.description
+            sleep 3 
+            gastronomy
+        
+        
+        # case answer
+        #   when "main menu"
+        #     main_menu
+        #   when  "exit"
+        #     goodbye
+        #   unless 0
+        #     puts "What is a(n) #{food_item.name}?"
+        #     puts food_item.description
+        #     sleep 3 
+        #     gastronomy
+        #   else
+        #     puts "I did not understand your selection.  Please try again."
+        #     gastronomy
+        #   end
+        # end
         
   #     #list of food items to choose from by number input
   #     @list_gastronomy = BrazilExperience::Gastronomy.list_gastronomy
