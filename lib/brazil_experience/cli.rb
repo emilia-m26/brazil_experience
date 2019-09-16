14fe1ed4b5dde976c18980858a5e7d2f740c75e9
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-16 00:14:58 +0000

    menus are double printing, have to fix issue

--- lib/brazil_experience/cli.rb
+++ lib/brazil_experience/cli.rb
@@ -5,52 +5,52 @@ class BrazilExperience::CLI
   def begin_experience
     puts "Welcome to".green.bold + " the Brazil".blue.bold + " Experience!".yellow.bold
       sleep 3
-    puts "What would you like to explore?".green.bold
+    puts "What would you like to explore?"
       sleep 2
-    #main_menu
-    #goodbye 
-    attractions
-    ##gastronomy
+    main_menu
+    goodbye 
     end
       
-  # def main_menu
+  def main_menu
     
-  #     puts "Please choose from: 
-  #         attractions, gastronomy or exit".green.bold
+      puts "Please choose from: 
+          attractions, gastronomy or exit"
           
-  #         answer = gets.strip.downcase
+          answer = gets.strip.downcase
 
-  #     case answer 
-  #       when "attractions"
-  #         attractions
-  #       when "gastronomy"
-  #         gastronomy
-  #       when "exit"
-  #         sleep 1 
-  #       else
-  #         puts "I did not understand your choice. Please try again.".green.bold
-  #           sleep 2
-  #       main_menu 
-  #     end
-  # end
+      case answer 
+        when "attractions"
+          attractions
+        when "gastronomy"
+          gastronomy
+        when "exit"
+          sleep 1 
+        else
+          puts "I did not understand your choice. Please try again."
+            sleep 2
+        main_menu 
+      end
+  end
   
   def attractions
-    puts "Here are your attractions. Which one would you like to learn more about?".blue.bold
+    puts "Here are your attractions. Which one would you like to learn more about?"
       sleep 1
-      Scraper.scrape_attractions
-    #puts "Please choose by number:".blue.bold
-    #   sleep 1
+    Scraper.scrape_attractions
+   
     BrazilExperience::Attractions.all.each_with_index do |attraction,index|
       puts "#{index + 1}. #{attraction.name}"
     end
      sleep 1
-    puts "Please choose by number:".yellow.bold
+    puts "Please choose by number:"
         answer = gets.strip.downcase
         
         attraction_item = BrazilExperience::Attractions.all[answer.to_i-1]
+        
         puts "#{attraction_item.name} is located in #{attraction_item.location}."
         
         puts attraction_item.description
+        sleep 2
+        
     #     #list of attractions to choose from by number input
     #     @list_attractions = BrazilExperience::Attractions.list_attractions
 
@@ -63,20 +63,24 @@ class BrazilExperience::CLI
   BrazilExperience::Gastronomy.all.each_with_index do |food,index|
     puts "#{index + 1}. #{food.name}"
   end
-     sleep 2
+     sleep 1
     puts "Please choose by number:".yellow.bold
         answer = gets.strip.downcase
-        
+        if answer.to_i > BrazilExperience::Gastronomy.all.size || answer.to_i <= 0
+          puts "I did not understand your selection.  Please try again."
+          main_menu 
+        end
+  
         food_item = BrazilExperience::Gastronomy.all[answer.to_i-1]
         puts "What is a(n) #{food_item.name}?"
         puts food_item.description
-        #binding.pry
+        
   #     #list of food items to choose from by number input
   #     @list_gastronomy = BrazilExperience::Gastronomy.list_gastronomy
     end
   
-  # def goodbye
-  #   puts "Thank you for visiting the Brazil Experience. We hope you enjoyed you stay.".blue.bold
-  # end
+  def goodbye
+    puts "Thank you for visiting the Brazil Experience. We hope you enjoyed you stay.".blue.bold
+  end
   
 end
